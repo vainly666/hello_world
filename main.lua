5b904b968892bfa0f1f1d58d8902242c34684846
d1064423025 <44631308+d1064423025@users.noreply.github.com>
2018-10-31 14:33:35 +0800

    line03
    03

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@ msgText:setFillColor( 0.2, 0.6, 0.8 )
 local msgText = display.newText( "Hello, world", world.x, world.y+80, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
 
-
+local msgText = display.newText( "Hello, world", world.x, world.y+40, native.systemFont, 32 )
+msgText:setFillColor( 0.2, 0.6, 0.8 )
 
 
